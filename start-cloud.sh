e9be79c7c046470c3caf12f7e576bcc07e6373c7
b9007082-cell <b9007082@gmail.com>
2026-09-10 10:00:26 +0800

    Fix cloud long-running API requests (start-cloud.sh)

--- start-cloud.sh
+++ start-cloud.sh
@@ -2,4 +2,5 @@
 set -eu
 .venv/bin/uvicorn api.main:app --host 127.0.0.1 --port 8000 &
 cd web
-exec npm run start -- --hostname 0.0.0.0 --port "${PORT:-10000}"
+npm run start -- --hostname 127.0.0.1 --port 3000 &
+exec nginx -g 'daemon off;'
